1ff51db5bd8b78c913052493612fad30e9c80362
Seija Ellum <sellum@smith.edu>
2026-09-21 13:10:43 -0400

    HW01 first commit

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
 # ------------------------------------------------------
-#        Name: (put your name here)
+#        Name: Seija Ellum
 #       Peers: (add any collaborators)
 #  References: (anything you checked to solve this)
 # ------------------------------------------------------
@@ -46,28 +46,58 @@ def main():
     # =============================================
     # Your code for part 1 under this line and before the print statements
 
+    #reassign variables
+    x = 27
+    y = 1
+    a = 1.5
+    b = 7
+    c = -1
+    result1 = (3 * x - 9 * y)/(2 * a *(b - c))
+    #print results
+    print("Part 1: x =", x)
+    print("Part 1: y =", y)
+    print("Part 1: a =", a)
+    print("Part 1: b =", b)
+    print("Part 1: c =", c)
+    print("Part 1: result =", result1)
     # End of Part 1 ----------------------
 
 
     # Part 2: Power
     # =============================================
     # Your code for part 2 under this line and before the print statements
-
+    
+    x = 5
+    y = -3
+    result2 = (x ** 2) * (y ** 4)
+    
+    print("Part 2: x =", x)
+    print("Part 2: y =", y)
+    print("Part 2: result =", result2)
     # End of Part 2 ----------------------
-
+    
 
 
     # Part 3: Integer divide
     # =============================================
     # Your code for part 3 under this line and before the print statements
-
+    
+    a = 100
+    b = 13
+    result3 = a//b
+    
+    print("Part 3: a =", a)
+    print("Part 3: b =", b)
+    print("Part 3: result =", result3)
     # End of Part 3 ----------------------
 
 
     # Part 4: Modulo
     # =============================================
     # Your code for part 4 under this line and before the print statements
-
+    
+    result4 = a%b
+    print("Part 4: result =", result4)
     # End of Part 4 ----------------------
 
 if __name__ == "__main__":
